4b8181f73140debaae811dd1c2c60630272aa56c
ike76 <ike76@me.com>
2018-04-21 23:01:38 -0500

    radix param

--- src/components/inputForm.js
+++ src/components/inputForm.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import './inputForm.css'
+import React from "react";
+import "./inputForm.css";
 export default function InputForm(props) {
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		let value = document.getElementById('guess').value
-		document.getElementById('guess').value = ''
-		props.onChange(parseInt(value))
-	}
-	if (!props.finished){
-		return (
-		<section >
-			<form action="#!"  className='form' onSubmit={handleSubmit}>
-				<input name='guess' id="guess" className="guessBox" type='number' />
-				<button type='submit'>Guess #{props.count +1}</button>
-			</form >
-		</section>
-		)
-	} else {
-		return (
-			<section className="finalSentence">
-				<h3 >You found the correct answer in <span className="bigNumber">{props.count}</span> guesses</h3>
-			</section>
-			)
-	}
+  const handleSubmit = e => {
+    e.preventDefault();
+    let value = document.getElementById("guess").value;
+    document.getElementById("guess").value = "";
+    props.onChange(parseInt(value, 10));
+  };
+  if (!props.finished) {
+    return (
+      <section>
+        <form action="#!" className="form" onSubmit={handleSubmit}>
+          <input name="guess" id="guess" className="guessBox" type="number" />
+          <button type="submit">Guess #{props.count + 1}</button>
+        </form>
+      </section>
+    );
+  } else {
+    return (
+      <section className="finalSentence">
+        <h3>
+          You found the correct answer in{" "}
+          <span className="bigNumber">{props.count}</span> guesses
+        </h3>
+      </section>
+    );
+  }
 }
